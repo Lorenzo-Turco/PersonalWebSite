d3d7e031432cb341f61299d7b8e622759e855f91
Lorenzo Turco <ltloreturco@gmail.com>
2023-11-12 12:42:37 +0100

    Added Language Switch V0.9

--- root/resources/js/index_script.js
+++ root/resources/js/index_script.js
@@ -2,9 +2,76 @@
 
 const currentPage = window.location.pathname.split("/").pop().split(".")[0];
 
-// Chiamare la funzione all'avvio della pagina
+// Oggetto delle traduzioni per testo multilingue
+var translations = {
+    "ita": {
+        "home": "Home",
+        "welcome": "Ciao! Sono Lorenzo Turco e questa è la mia Home Page. Questo sito è una versione in pre-alfa e seguiranno aggiornamenti!",
+        "warning": "Attenzione!",
+        "warning-initial-popup": "Possibili bug di navigazione in pre-alfa",
+        "change-logs": "Change Logs",
+        "about-me": "Su di Me",
+        "code": "Codice",
+        "videogame": "Videogame",
+        "resources": "Risorse",
+        "wip": "Lavori in corso...",
+        "code-home-page": "Qui puoi trovare le mie repository pubbliche, come codici Python (simulazioni finanziarie e di fisica), Salesforce, ecc...",
+        "videogame-home-page": "Progetto di Videogame in corso... Sto ancora imparando Unreal Engine 4",
+        "resources-home-page": "Qui puoi trovare i link ai miei documenti/certificazioni/CV, ecc...",
+        "change-logs-home-page": "Qui puoi trovare lo storico delle versioni del sito",
+        "change-logs-version-tag": "Versione",
+        "change-logs-date-tag": "Data",
+        "change-logs-desc-tag": "Descrizione",
+        "python-tag": "Pyhton",
+        "physics-tag": "Fisica",
+        "finance-tag": "Finanza",
+        "physics-sim-gr-earth-tag": "Simulazioni di fisica - Moto della Terra in GR",
+        "physics-sim-gr-earth-desc": "Simulazione della traiettoria della Terra intorno al Sole, con una correzzione enfatizzata di Relatività Generale",
+        "finance-lstm-sims-tag": "Predizioni LSTM di prezzo di asset finanziari",
+        "finance-lstm-sims-desc": "Predizione di prezzo di asset nel tempo in un modello LSTM",
+        "montecarlo-sims-tag": "Simulazioni Montecarlo di Portfogli",
+        "montecarlo-sims-desc": "Simulazione Montecarlo per ottimizzare l'allocazione di asset",
+        "resources-github-tag": "Documenti GitHub",
+        "resources-cv-tag": "CV",
+        "resources-certifications-tag": "Certificazioni"
+    },
+    "eng": {
+        "home": "Home",
+        "welcome": "Hi! I'm Lorenzo Turco and this is my Home Page. This is a pre-alpha site version and updates will follow!",
+        "warning": "Warning!",
+        "warning-initial-popup": "Possible pre-alpha navigation bugs",
+        "change-logs": "Change Logs",
+        "about-me": "About Me",
+        "code": "Code",
+        "videogame": "Videogame",
+        "resources": "Resources",
+        "wip": "Work in progress...",
+        "code-home-page": "Here you can find my public projects, such as Pyhton (finance and physics simulations), Salesforce codes, and so on...",
+        "videogame-home-page": "Videogame project Work in Progress... I\'m still learning Unreal Engine 4",
+        "resources-home-page": "Here you can find links to my documents/credentials/CV, and so on...",
+        "change-logs-home-page": "Here you can see the history site versioning",
+        "change-logs-version-tag": "Version",
+        "change-logs-date-tag": "Date",
+        "change-logs-desc-tag": "Description",
+        "python-tag": "Pyhton",
+        "physics-tag": "Physics",
+        "finance-tag": "Finance",
+        "physics-sim-gr-earth-tag": "Physics Simulations - GR Earth motion",
+        "physics-sim-gr-earth-desc": "Simulation of Earth trajectory around Sun, with emphasized General Relativity correction",
+        "finance-lstm-sims-tag": "Finance LSTM assets price predictions",
+        "finance-lstm-sims-desc": "Prediction of asset price over time with LSTM model",
+        "montecarlo-sims-tag": "Montecarlo Portfoglio Simulations",
+        "montecarlo-sims-desc": "Portfoglio Montecarlo simulation for opmimize the assets allocation",
+        "resources-github-tag": "GitHub Documents",
+        "resources-cv-tag": "CV",
+        "resources-certifications-tag": "Certifications"
+    }
+};
+
+// Chiamare le funzioni all'avvio della pagina
 window.addEventListener('DOMContentLoaded', function() {
     setThemeFromSessionStorage(); // Applica il tema memorizzato immediatamente
+    setLanguageFromSessionStorage(); // Imposta la lingua al caricamento della pagina
 });
 
 var themeToggle = document.getElementById('theme-toggle');
@@ -113,7 +180,7 @@ function darkMode() {
     }
     themeToggle.style.backgroundColor = '#002eff'; // Sfondo giallo per tema notturno
     themeToggle.style.color = itemLightColor; // Cambia il colore del testo per tema notturno
-    containerTheme.style.background = `linear-gradient(to right, #818181, rgb(165, 100, 100))`;
+    containerTheme.style.background = `linear-gradient(to right, #000, rgb(165, 100, 100))`;
     containerTheme.style.color = itemDarkColor; // Testo bianco per tema diurno
     headerTheme.style.background = `linear-gradient(45deg, rgb(165, 100, 100), rgb(100, 120, 150))`;
     headerTheme.style.color = itemDarkColor; // Cambia il colore del testo per tema notturno
@@ -147,4 +214,40 @@ function darkMode() {
         }
     }
     sessionStorage.setItem('theme', 'dark');
+}
+
+// Funzione per aggiornare la lingua quando si clicca su una bandiera
+function updateLanguage(flag) {
+    document.querySelectorAll('.language-flag').forEach(function(f) {
+        if (f !== flag) {
+            f.classList.add('inactive-flag');
+        } else {
+            f.classList.remove('inactive-flag');
+            sessionStorage.setItem('currentLanguage', flag.getAttribute('data-lang'));
+            updatePageLanguage(); // Aggiorna il testo nella nuova lingua
+        }
+    });
+}
+
+// Gestore di eventi per il cambio di lingua tramite bandiere
+document.querySelectorAll('.language-flag').forEach(function(flag) {
+    flag.addEventListener('click', function() {
+        updateLanguage(flag);
+    });
+});
+
+// Funzione per impostare la lingua in base a sessionStorage
+function setLanguageFromSessionStorage() {
+    updatePageLanguage();
+}
+
+// Funzione per aggiornare il testo in base alla lingua corrente
+function updatePageLanguage() {
+    var currentLanguage = sessionStorage.getItem('currentLanguage') || 'ita';
+    var elements = document.querySelectorAll('[data-translate]');
+    
+    elements.forEach(function(element) {
+        var key = element.getAttribute('data-translate');
+        element.innerHTML = translations[currentLanguage][key] || key;
+    });
 }
